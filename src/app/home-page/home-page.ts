f6719dcf411ab7c0ee24308cebd1ecb9ad8265b1
Kasia Jankowska <katarzyna.jankowska000728@gmail.com>
2026-06-11 11:51:34 +0200

    Style buttons active route buttons

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatButtonModule],
   templateUrl: './home-page.html',
 })
-export class HomePage {}
+export class HomePage {
+  constructor(private router: Router) {}
+
+  isActive(path: string) {
+    return this.router.url === path;
+  }
+}
